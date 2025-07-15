edfc52c076e19a1ff5830bf12ecfb6611916343a
mrkevyn <85470079+mrkevyn@users.noreply.github.com>
2025-07-15 17:01:13 -0300

    Update ProjetosRepository.java
    Att

--- src/main/java/semit/saoluis/repository/ProjetosRepository.java
+++ src/main/java/semit/saoluis/repository/ProjetosRepository.java
@@ -1,9 +1,15 @@
 package semit.saoluis.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
 import semit.saoluis.model.Projetos;
 
 @Repository
 public interface ProjetosRepository extends JpaRepository<Projetos, Long> {
+	
+	List<Projetos> findByIsVisibleTrue();
 }
+
